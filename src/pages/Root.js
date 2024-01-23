042db460fbf6636a704cb9ea780d10ddf7a3e4d3
pavan <pavan49450@gmail.com>
2024-01-23 20:05:32 +0530

    Added analytics method in project

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Layout/Header";
 import style from "./Root.module.css";
 import Footer from "../components/Layout/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const RootLayout = () => {
   return (
     <div className={style.root}>
       <Header />
       <main className={style.main}>
+        <Analytics />
         <Outlet />
       </main>
       <Footer />
