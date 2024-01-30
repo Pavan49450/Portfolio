10779efd5f14f998b3892a2e0336dddcf346d150
pavan <pavan49450@gmail.com>
2024-01-30 10:24:58 +0530

    Added the Azure deployed backend URL

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,5 +1,6 @@
 const URL = {
-  backendUrl: "https://kattula-pavan-kumar-portfolio-backend.onrender.com",
+  // backendUrl: "https://kattula-pavan-kumar-portfolio-backend.onrender.com",
+  backendUrl: "https://kpk-portfolio-backend.azurewebsites.net",
   // backendUrl: "http://localhost:3001",
 };
 export default URL;
