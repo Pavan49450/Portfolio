a1048f63b364e9e367c619bd11d1b7e2d49b14c9
pavan <pavank@spileplus.com>
2025-01-30 16:05:02 +0530

    removed the extra slash

--- src/constants/url.js
+++ src/constants/url.js
@@ -2,6 +2,6 @@ const URL = {
   // backendUrl: "https://kattula-pavan-kumar-portfolio-backend.onrender.com",
   // backendUrl: "https://kpk-portfolio-backend.azurewebsites.net",
   // backendUrl: "http://localhost:3001",
-  backendUrl: "https://portfolio-backend-theta-steel.vercel.app/",
+  backendUrl: "https://portfolio-backend-theta-steel.vercel.app",
 };
 export default URL;
