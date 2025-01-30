053b1355b8a2747ee95bcdfd09e98f1e3a38e566
pavan <pavank@spileplus.com>
2025-01-30 16:01:55 +0530

    updated the backend URL

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,6 +1,7 @@
 const URL = {
   // backendUrl: "https://kattula-pavan-kumar-portfolio-backend.onrender.com",
   // backendUrl: "https://kpk-portfolio-backend.azurewebsites.net",
-  backendUrl: "http://localhost:3001",
+  // backendUrl: "http://localhost:3001",
+  backendUrl: "https://portfolio-backend-theta-steel.vercel.app/",
 };
 export default URL;
